32694be5dd000c6ee940cb69679e58cf79eaa101
David Stanley <dstanley@uoguelph.ca>
2015-10-09 15:21:23 -0400

    add data

---
+++ R/data-example1.R
@@ -0,0 +1,15 @@
+#' @title Example 1 Data (n=100)
+#' @docType data
+#' @keywords datasets
+#' @name example1
+#' @usage data(example1)
+#' @format A data frame with 100 rows and 2 variables:
+#' \describe{
+#'   \item{SelfEsteem}{Self-Esteem scores }
+#'   \item{Height}{Heights of participants}
+#' }
+"example1"
+NULL
+
+
+
---
+++ R/data-example2.R
@@ -0,0 +1,15 @@
+#' @title Example 2 Data (n=75)
+#' @docType data
+#' @keywords datasets
+#' @name example2
+#' @usage data(example2)
+#' @format A data frame with 75 rows and 2 variables:
+#' \describe{
+#'   \item{SelfEsteem}{Self-Esteem scores }
+#'   \item{Height}{Heights of participants}
+#' }
+"example2"
+NULL
+
+
+
